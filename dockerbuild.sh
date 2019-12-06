d5ff0b77d790ccec423a0d4adfb87b8686f3f607
Seth Bergman <sethbergman@users.noreply.github.com>
2019-12-06 03:00:51 -0600

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 echo Building nodewebapp from Dockerfile.dev
 
-docker build -t nodewebapp:v1 -f Dockerfile.dev .
+docker build -t $CI_PROJECT_NAME:$CI_COMMIT_REF -f Dockerfile.dev .
 
 echo Creating Conatiner Out of Image
-docker container create --name extract nodewebapp:v1
+docker container create --name extract $CI_PROJECT_NAME:$CI_COMMIT_REF
 docker container cp extract:/usr/src/app/WebApp/dist ./dist
 docker container rm -f extract
 
 echo Building nodewebapp version 2
 
-docker build --no-cache -t nodewebapp:v2 .
+docker build --no-cache -t $CI_PROJECT_NAME:$CI_COMMIT_REF .
 rm ./dist
